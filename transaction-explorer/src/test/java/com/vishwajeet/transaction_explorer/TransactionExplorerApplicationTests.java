0ed76eb86569434fbc0d1dfe3007beddbc2f2f40
VISHWAJEET KUMAR <vishwajeetkr96@gmail.com>
2026-04-18 18:00:15 +0530

    initial project structure from Spring Initializr

---
+++ transaction-explorer/src/test/java/com/vishwajeet/transaction_explorer/TransactionExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishwajeet.transaction_explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
